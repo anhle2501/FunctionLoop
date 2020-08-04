c25ab10991bb4aa71c6b611baa1dae802e38c66d
Anh Le Nhut <nhut.anh2501@gmail.com>
2020-08-04 22:14:07 +0700

    4.8.2020 lt xong

--- FunctionLoop/Program.cs
+++ FunctionLoop/Program.cs
@@ -22,9 +22,75 @@ namespace FunctionLoop
             return (Math.Pow(x, n)) / giaithua(n);
         }
 
+        // định nghĩa hàm tính công thức S1(x)
+        static double S1(double x)
+        {
+            // lúc đầu thiết lập S=1 và n=1
+            double S = 1;
+            int n = 1;
+            double En;
+            do
+            {
+                // tính En tích lũy En vào S
+                En = EnS1(x, n);
+                S = S + En;
+                n++;
+            } while (Math.Abs(En) >= EPSILON); // lắp lại bước 2 nếu En>=epsilon
+            return S; // gởi kết quả về lệnh này
+        }
+        
+        // định nghĩa hàm thành paahfn (-1)^nx^n/n!
+        static double EnS2(double x, int n)
+        {
+            return Math.Pow(-1, n) * Math.Pow(x, n) / giaithua(n);
+        }
+
+        // định nghĩa hàm tính công thức S2(x)
+        static double S2(double x)
+        {
+            //thiết lập S = 1 và n = 1
+            double S = 1;
+            int n = 1;
+            double En;
+            do
+            {
+                //tính En tích lũy En vào S
+                En = EnS2(x, n);
+                S = S + En;
+                n++;
+            } while (Math.Abs(En) >= EPSILON); // lặp lại nếu En >= epsilon
+            return S;
+        }
+
+        static double S3(double x)
+        {
+            double S=1;
+            int n = 1;
+            double En;
+            do
+            {
+                En = Math.Sin(n * x);
+                S = S + En;
+                n++;
+            } while (Math.Abs(En) >= EPSILON);
+            return S;
+        }
+
+        // chương trình dùng thử
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x;
+            String buf;
+            // yêu cầu người dùng nhập x
+            Console.Write("Nhap x:");
+            buf = Console.ReadLine();
+            x = Double.Parse(buf);
+            //gọi hàm S1(x)
+            Console.WriteLine("S1(" + x + ")=" + S1(x));
+            //gọi hàm S2(x)
+            Console.WriteLine("S2(" + x + ")=" + S2(x));
+            //gọi hàm S3(x)
+            Console.WriteLine("S3(" + x + ")=" + S3(x));
         }
     }
 }
